bd4a940c94b41ee2b6f595854ea2565e0a76a73e
Paul Dicker <pitdicker@gmail.com>
2019-10-21 11:07:38 +0200

    Attempt to detect reentrant initialization using ReentrantMutex

--- src/imp_pl.rs
+++ src/imp_pl.rs
@@ -1,14 +1,15 @@
 use std::{
     cell::UnsafeCell,
     panic::{RefUnwindSafe, UnwindSafe},
-    sync::atomic::{AtomicBool, Ordering},
+    sync::atomic::{AtomicUsize, Ordering},
+    mem,
 };
 
 use parking_lot::{lock_api::RawMutex as _RawMutex, RawMutex};
 
 pub(crate) struct OnceCell<T> {
-    mutex: Mutex,
-    is_initialized: AtomicBool,
+    mutex: ReentrantMutex,
+    state: AtomicUsize,
     pub(crate) value: UnsafeCell<Option<T>>,
 }
 
@@ -23,11 +24,16 @@ unsafe impl<T: Send> Send for OnceCell<T> {}
 impl<T: RefUnwindSafe + UnwindSafe> RefUnwindSafe for OnceCell<T> {}
 impl<T: UnwindSafe> UnwindSafe for OnceCell<T> {}
 
+// Three states that a OnceCell can be in.
+const EMPTY: usize = 0x2;
+const RUNNING: usize = 0x1;
+const COMPLETE: usize = 0x0;
+
 impl<T> OnceCell<T> {
     pub(crate) const fn new() -> OnceCell<T> {
         OnceCell {
-            mutex: Mutex::new(),
-            is_initialized: AtomicBool::new(false),
+            mutex: ReentrantMutex::new(),
+            state: AtomicUsize::new(EMPTY),
             value: UnsafeCell::new(None),
         }
     }
@@ -35,33 +41,70 @@ impl<T> OnceCell<T> {
     /// Safety: synchronizes with store to value via Release/Acquire.
     #[inline]
     pub(crate) fn is_initialized(&self) -> bool {
-        self.is_initialized.load(Ordering::Acquire)
+        self.state.load(Ordering::Acquire) == COMPLETE
     }
 
-    /// Safety: synchronizes with store to value via `is_initialized` or mutex
+    /// Safety: synchronizes with store to value via `state` or mutex
     /// lock/unlock, writes value only once because of the mutex.
     #[cold]
     pub(crate) fn initialize<F, E>(&self, f: F) -> Result<(), E>
     where
         F: FnOnce() -> Result<T, E>,
     {
+        if self.is_initialized() {
+            return Ok(());
+        }
+        // Lock a mutex so other threads can wait until we are ready.
         let _guard = self.mutex.lock();
-        if !self.is_initialized() {
-            // We are calling user-supplied function and need to be careful.
-            // - if it returns Err, we unlock mutex and return without touching anything
-            // - if it panics, we unlock mutex and propagate panic without touching anything
-            // - if it calls `set` or `get_or_try_init` re-entrantly, we get a deadlock on
-            //   mutex, which is important for safety. We *could* detect this and panic,
-            //   but that is more complicated
-            // - finally, if it returns Ok, we store the value and store the flag with
-            //   `Release`, which synchronizes with `Acquire`s.
-            let value = f()?;
-            let slot: &mut Option<T> = unsafe { &mut *self.value.get() };
-            debug_assert!(slot.is_none());
-            *slot = Some(value);
-            self.is_initialized.store(true, Ordering::Release);
+        let old = self.state.compare_and_swap(EMPTY, RUNNING, Ordering::Relaxed);
+        match old {
+            EMPTY => {
+                // This thread is the only one currently trying to initialize the Cell,
+                // and we just set `state` to RUNNING.
+                let reset_if_failed = store_on_drop(&self.state, EMPTY, Ordering::Relaxed);
+                let value = f()?;
+                let slot: &mut Option<T> = unsafe { &mut *self.value.get() };
+                debug_assert!(slot.is_none());
+                *slot = Some(value);
+                reset_if_failed.cancel();
+                self.state.store(COMPLETE, Ordering::Release);
+            }
+            RUNNING => {
+                // We were able to lock the ReentrantMutex wile an initializer is running.
+                // This must be a case of reentrant initialization.
+                panic!();
+            }
+            COMPLETE => {} // Another thread must have finished initializing the Cell just now.
         }
         Ok(())
+    }
+}
+
+// Helper struct used to clean up after a closure call with a `Drop`
+// implementation to also run on panic.
+struct StoreOnDrop<'a> {
+    atomic: &'a AtomicUsize,
+    value: usize,
+    ordering: Ordering,
+}
+
+fn store_on_drop<'a>(atomic: &'a AtomicUsize, value: usize, ordering: Ordering) -> StoreOnDrop<'a> {
+    StoreOnDrop {
+        atomic,
+        value,
+        ordering,
+    }
+}
+
+impl StoreOnDrop<'_> {
+    fn cancel(self) {
+        mem::forget(self)
+    }
+}
+
+impl Drop for StoreOnDrop<'_> {
+    fn drop(&mut self) {
+        self.atomic.store(self.value, self.ordering)
     }
 }
 
